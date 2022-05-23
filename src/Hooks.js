076ff9a0524e191e15dcada20d5e10cdfab03499
VigneshkarthickR <rvkarthick97@gmail.com>
2022-05-23 11:59:57 +0530

    use state

---
+++ src/Hooks.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+function Hooks() {
+    let [count, setCount] = useState(10);
+    return <p onClick={() => {
+        setCount(count + 1)
+    }}>{count}</p>
+}
+
+
+export default Hooks
